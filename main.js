05415fb0350c08270427edb0ae6593c912f2ecbc
Marcus <jrdevleague12@Juniors-iMac-9.local>
2016-11-18 16:37:53 -1000

    added basic code

---
+++ main.js
@@ -0,0 +1,17 @@
+var score = 0;
+
+function sause(){
+	console.log("Hello");
+	console.log(document.getElementById("tomato"));
+  var pTagById = document.getElementById("tomato")
+
+	pTagById.innerHTML = score;
+
+	score++;
+console.log(score);
+
+
+
+
+
+}
